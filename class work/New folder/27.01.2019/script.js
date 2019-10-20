e17c4b2ad251927a702a424eb498b0fb5dcf3a8f
zulqarnainnaviwala <zulqarnainnaviwala@gmail.com>
2019-10-20 20:56:45 +0500

    08.09.2018

---
+++ class work/New folder/27.01.2019/script.js
@@ -0,0 +1,144 @@
+
+
+
+// var studentsName = ["shahid" , "bilal" , "mubashir" , "ali" , "ahmed"];
+
+// alert(studentsName);
+
+
+
+/////////////////////////////////////////////////////////////////////
+
+
+// var numA = [ 1 , 2 , 3 , 4 ,5 , 6]
+
+// alert(numA);
+
+
+
+////////////////////////////////////////////////////////////////
+
+
+
+// var booleanArray = [true , false ];
+
+// alert(booleanArray);
+
+
+
+////////////////////////////////////////////////////////////////
+
+
+
+
+// var mixArray = [1 ,"Shahid", true , 2 , "Mubashir" ,false ];
+
+// alert(mixArray);
+
+
+
+////////////////////////////////////////////////////////////////
+
+
+
+// var mobNetworks = ["Jazz" , "Warid" , "Zong" , "Telenor" ]
+
+// alert(mobNetworks);
+
+
+
+////////////////////////////////////////////////////////////////
+
+
+
+// var eduPak = ["SSC", "HSC" , "BCS" , "BS", "BCOM" , "MS", "M" , "Phil" , "PhD"]
+
+// document.write("<h1> Qualifications... </h1>");
+
+// document.write("<ol>" +
+// "<li>" + eduPak[0] +"</li>" + "<li>" + eduPak[1] +"</li>" +"<li>" 
+// + eduPak[2] +"</li>" +"<li>" + eduPak[3] +"</li>" +"<li>" 
+// + eduPak[4] +"</li>" +"<li>" + eduPak[5] +"</li>" +"<li>" +
+//  eduPak[6] +"</li>" +"<li>" + eduPak[7] +"</li>" +"<li>" +
+//   eduPak[8] )
+
+
+
+
+////////////////////////////////////////////////////////////////
+
+
+
+
+var colorNames = ["black", "cream" , "white" , "yellow", "pink" , "purple", "green" , "blue" , "red"]
+
+document.write("<h1> Color Names... </h1>");
+document.write("<ol>" +
+"<li>" + colorNames[0] +"</li>" + "<li>" + colorNames[1] +"</li>" +"<li>" 
++ colorNames[2] +"</li>" +"<li>" + colorNames[3] +"</li>" +"<li>" 
++ colorNames[4] +"</li>" +"<li>" + colorNames[5] +"</li>" +"<li>" +
+colorNames[6] +"</li>" +"<li>" + colorNames[7] +"</li>" +"<li>" +
+colorNames[8] +"</li>" )
+
+
+
+var userColor1 = prompt("What Color Name you Want To put in the beginning of the Array ? ");
+colorNames.unshift(userColor1)
+
+document.write("<h1> Color Names... </h1>");
+document.write("<ol>" +
+"<li>" + colorNames[0] +"</li>" + "<li>" + colorNames[1] +"</li>" +"<li>" 
++ colorNames[2] +"</li>" +"<li>" + colorNames[3] +"</li>" +"<li>" 
++ colorNames[4] +"</li>" +"<li>" + colorNames[5] +"</li>" +"<li>" +
+colorNames[6] +"</li>" +"<li>" + colorNames[7] +"</li>" +"<li>" +
+colorNames[8] + "<li>" + colorNames[9] +"</li>" )
+
+
+var userColor2 = prompt("What Color Name you Want To put in the end of the Array ? ");
+
+
+document.write("<h1> Color Names... </h1>");
+document.write("<ol>" +
+"<li>" + colorNames[0] +"</li>" + "<li>" + colorNames[1] +"</li>" +"<li>" 
++ colorNames[2] +"</li>" +"<li>" + colorNames[3] +"</li>" +"<li>" 
++ colorNames[4] +"</li>" +"<li>" + colorNames[5] +"</li>" +"<li>" +
+colorNames[6] +"</li>" +"<li>" + colorNames[7] +"</li>" +"<li>" +
+colorNames[8] + "<li>" + colorNames[9] +"</li>" + "<li>" + colorNames[10] +"</li>" )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////
